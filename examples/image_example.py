13bf0598f24fdad1ab54b6739d221f685e7907db
vinpap2008S <mirzoevsergej881@gmail.com>
2023-11-17 17:38:30 +0300

    приложение начало

--- examples/image_example.py
+++ examples/image_example.py
@@ -4,18 +4,25 @@ from PIL import Image
 
 
 class App(customtkinter.CTk):
+    
+    
     def __init__(self):
         super().__init__()
-
+        global framgl_name,fram1_name,fram2_name,fram3_name
+        framgl_name = "Главная"
+        fram1_name = "Добавление обьектов"
+        fram2_name = "Профиль"
+        fram3_name = ""
+        # Создание окна
         self.title("image_example.py")
         self.geometry("700x450")
 
-        # set grid layout 1x2
+        # установиливаю макет сетки 1x2
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=1)
 
-        # load images with light and dark mode image
-        image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "test_images")
+        # Загрузка изображений
+        image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "test_images") #Изображение поиск
         self.logo_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, "CustomTkinter_logo_single.png")), size=(26, 26))
         self.large_test_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, "large_test_image.png")), size=(500, 150))
         self.image_icon_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, "image_icon_light.png")), size=(20, 20))
@@ -26,7 +33,7 @@ class App(customtkinter.CTk):
         self.add_user_image = customtkinter.CTkImage(light_image=Image.open(os.path.join(image_path, "add_user_dark.png")),
                                                      dark_image=Image.open(os.path.join(image_path, "add_user_light.png")), size=(20, 20))
 
-        # create navigation frame
+        # Навигация
         self.navigation_frame = customtkinter.CTkFrame(self, corner_radius=0)
         self.navigation_frame.grid(row=0, column=0, sticky="nsew")
         self.navigation_frame.grid_rowconfigure(4, weight=1)
@@ -34,82 +41,86 @@ class App(customtkinter.CTk):
         self.navigation_frame_label = customtkinter.CTkLabel(self.navigation_frame, text="  Image Example", image=self.logo_image,
                                                              compound="left", font=customtkinter.CTkFont(size=15, weight="bold"))
         self.navigation_frame_label.grid(row=0, column=0, padx=20, pady=20)
-
-        self.home_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Home",
+        # 1 фрейм
+        self.home_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text=framgl_name,
                                                    fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
                                                    image=self.home_image, anchor="w", command=self.home_button_event)
         self.home_button.grid(row=1, column=0, sticky="ew")
-
-        self.frame_2_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Frame 2",
+        # 2 фрейм
+        self.frame_2_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text=fram1_name,
                                                       fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
                                                       image=self.chat_image, anchor="w", command=self.frame_2_button_event)
         self.frame_2_button.grid(row=2, column=0, sticky="ew")
 
-        self.frame_3_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Frame 3",
+        # 3 фрейм
+        self.frame_3_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text=fram2_name,
                                                       fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
                                                       image=self.add_user_image, anchor="w", command=self.frame_3_button_event)
         self.frame_3_button.grid(row=3, column=0, sticky="ew")
-
-        self.appearance_mode_menu = customtkinter.CTkOptionMenu(self.navigation_frame, values=["Light", "Dark", "System"],
-                                                                command=self.change_appearance_mode_event)
+        # выбор темы
+        self.appearance_mode_menu = customtkinter.CTkOptionMenu(self.navigation_frame, values=["Светлая", "Тёмная", "Системная"],command=self.change_appearance_mode_event)
         self.appearance_mode_menu.grid(row=6, column=0, padx=20, pady=20, sticky="s")
 
-        # create home frame
+        # Главный фрейм
         self.home_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
         self.home_frame.grid_columnconfigure(0, weight=1)
 
         self.home_frame_large_image_label = customtkinter.CTkLabel(self.home_frame, text="", image=self.large_test_image)
         self.home_frame_large_image_label.grid(row=0, column=0, padx=20, pady=10)
 
-        self.home_frame_button_1 = customtkinter.CTkButton(self.home_frame, text="", image=self.image_icon_image)
-        self.home_frame_button_1.grid(row=1, column=0, padx=20, pady=10)
-        self.home_frame_button_2 = customtkinter.CTkButton(self.home_frame, text="CTkButton", image=self.image_icon_image, compound="right")
-        self.home_frame_button_2.grid(row=2, column=0, padx=20, pady=10)
-        self.home_frame_button_3 = customtkinter.CTkButton(self.home_frame, text="CTkButton", image=self.image_icon_image, compound="top")
-        self.home_frame_button_3.grid(row=3, column=0, padx=20, pady=10)
-        self.home_frame_button_4 = customtkinter.CTkButton(self.home_frame, text="CTkButton", image=self.image_icon_image, compound="bottom", anchor="w")
-        self.home_frame_button_4.grid(row=4, column=0, padx=20, pady=10)
 
-        # create second frame
+
+
+        # создаем фрейм
         self.second_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
 
-        # create third frame
+        # создаем фрейм
         self.third_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
 
-        # select default frame
-        self.select_frame_by_name("home")
+        # Настройка
+
+        self.home_frame_frame_2_entry = customtkinter.CTkEntry(self.third_frame)
+        self.home_frame_frame_2_entry.grid()
+
+        # Главный фрейм
+        self.select_frame_by_name(framgl_name)
 
     def select_frame_by_name(self, name):
-        # set button color for selected button
-        self.home_button.configure(fg_color=("gray75", "gray25") if name == "home" else "transparent")
-        self.frame_2_button.configure(fg_color=("gray75", "gray25") if name == "frame_2" else "transparent")
-        self.frame_3_button.configure(fg_color=("gray75", "gray25") if name == "frame_3" else "transparent")
+        # Цвет кнопки от выбора
+        self.home_button.configure(fg_color=("gray75", "gray25") if name == framgl_name else "transparent")
+        self.frame_2_button.configure(fg_color=("gray75", "gray25") if name == fram1_name else "transparent")
+        self.frame_3_button.configure(fg_color=("gray75", "gray25") if name == fram2_name else "transparent")
 
-        # show selected frame
-        if name == "home":
+        # Показ выбранного
+        if name == framgl_name:
             self.home_frame.grid(row=0, column=1, sticky="nsew")
         else:
             self.home_frame.grid_forget()
-        if name == "frame_2":
+        if name == fram1_name:
             self.second_frame.grid(row=0, column=1, sticky="nsew")
         else:
             self.second_frame.grid_forget()
-        if name == "frame_3":
+        if name == fram1_name:
             self.third_frame.grid(row=0, column=1, sticky="nsew")
         else:
             self.third_frame.grid_forget()
 
     def home_button_event(self):
-        self.select_frame_by_name("home")
+        self.select_frame_by_name(framgl_name)
 
     def frame_2_button_event(self):
-        self.select_frame_by_name("frame_2")
+        self.select_frame_by_name(fram1_name)
 
     def frame_3_button_event(self):
-        self.select_frame_by_name("frame_3")
+        self.select_frame_by_name(fram2_name)
 
     def change_appearance_mode_event(self, new_appearance_mode):
-        customtkinter.set_appearance_mode(new_appearance_mode)
+        if new_appearance_mode == 'Светлая':
+            customtkinter.set_appearance_mode("light")
+        elif new_appearance_mode == 'Тёмная':
+            customtkinter.set_appearance_mode("dark")
+        else:
+            customtkinter.set_appearance_mode("system")
 
 
 if __name__ == "__main__":
--- Проверка.py
+++ Проверка.py
@@ -1,6 +1,4 @@
 import customtkinter
-
-
 class MyCheckboxFrame(customtkinter.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
@@ -9,7 +7,8 @@ class MyCheckboxFrame(customtkinter.CTkFrame):
         self.checkbox_1.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="w")
         self.checkbox_2 = customtkinter.CTkCheckBox(self, text="checkbox 2")
         self.checkbox_2.grid(row=1, column=0, padx=10, pady=(10, 0), sticky="w")
-
+        self.checkbox_3 = customtkinter.CTkCheckBox(self, text="checkbox 3")
+        self.checkbox_3.grid(row=2, column=0, padx=10, pady=(10, 0), sticky="w")
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
