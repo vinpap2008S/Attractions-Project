27e147da159f0f1d51d43fd40270b1ea31f5e2b8
vinpap2008S <mirzoevsergej881@gmail.com>
2023-12-01 22:27:21 +0300

    приложение 3

--- opisanie.py
+++ opisanie.py
@@ -7,28 +7,20 @@ conn = sqlite3.connect('cities.db')
 conn.execute('''CREATE TABLE IF NOT EXISTS cities
              (id INTEGER PRIMARY KEY AUTOINCREMENT,
              name TEXT NOT NULL,
-             description TEXT NOT NULL);''')
+             description TEXT NOT NULL,
+             location TEXT NOT NULL);''')
 
-def add_city(name, description):
-    # Добавляем новый город в базу данных
-    conn.execute("INSERT INTO cities (name, description) VALUES (?, ?)", (name, description))
+def add_city(name, description, location=""):
+    # Добавляем новый город описание в базу данных
+    conn.execute("INSERT INTO cities (name, description, location) VALUES (?, ?, ?)", (name, description, location))
     conn.commit()
     print("Город успешно добавлен!")
 
-def get_city_info(name):
-    # Получаем информацию о городе по его названию
-    cursor = conn.execute("SELECT description FROM cities WHERE name=?", (name,))
-    city_info = cursor.fetchone()
-    if city_info:
-        print(f"Описание города {name}: {city_info[0]}")
-    else:
-        print("Город не найден!")
-
 # Пример использования функций
-add_city("Москва", "Столица России")
-add_city("Санкт-Петербург", "Город на Неве")
-get_city_info("Москва")
-get_city_info("Санкт-Петербург")
+# add_city("Москва", "Столица России", "Центральная Россия")
+# add_city("Санкт-Петербург", "Город на Неве", "Северо-Западная Россия")
+# check_city("Москва")
+# check_city("Екатеринбург")
 
 # Закрываем соединение с базой данных
 conn.close()
--- siti.py
+++ siti.py
@@ -1,22 +1,22 @@
 import sqlite3
 
 # Создаем подключение к базе данных или открываем существующую
-conn = sqlite3.connect('cities.db')
+site = sqlite3.connect('cities.db')
 
 # Создаем таблицу, если она не существует
-conn.execute('''CREATE TABLE IF NOT EXISTS cities
+site.execute('''CREATE TABLE IF NOT EXISTS cities
              (id INTEGER PRIMARY KEY AUTOINCREMENT,
              name TEXT NOT NULL);''')
 
 def add_city(name):
     # Добавляем новый город в базу данных
-    conn.execute("INSERT INTO cities (name) VALUES (?)", (name,))
-    conn.commit()
+    site.execute("INSERT INTO cities (name) VALUES (?)", (name,))
+    site.commit()
     print("Город успешно добавлен!")
 
 def check_city(name):
     # Проверяем наличие города в базе данных
-    cursor = conn.execute("SELECT name FROM cities WHERE name=?", (name,))
+    cursor = site.execute("SELECT name FROM cities WHERE name=?", (name,))
     city = cursor.fetchone()
     if city:
         print(f"Город {name} найден в базе данных!")
@@ -31,4 +31,4 @@ def check_city(name):
 
 # Закрываем соединение с базой данных
 
-conn.close()
+site.close()
