219fd3faa4220b51957c9f02cd30e5937b223384
vinpap2008S <mirzoevsergej881@gmail.com>
2023-12-01 21:21:33 +0300

    приложение 3

--- хочу не хочу.py
+++ хочу не хочу.py
@@ -1,14 +1,16 @@
 p = open('sitis.txt', 'r', encoding="utf8")
 a = []
 d = 0
+k = 0
 for line in p.readlines():
     if len(line) >= 3:
+        k+=1
         if a.count(line[:-1]) == 1:
             d+=1
+            print(k)
         else:
             a.append(line[:-1])
 
 print(a)
 print(d)
-
 p.close()
