812f7fded8718a5ba32039d6591770eeefafe212
vinpap2008S <mirzoevsergej881@gmail.com>
2023-12-02 21:31:52 +0300

    приложение 3

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from PIL import Image
 import os
 import sqlite3
 
-import customtkinter
+from customtkinter import *
 
 from globall import *
 def add_city(name):# Добавляем новый город в базу данных
@@ -67,16 +67,6 @@ opisenie_cursor.execute('''CREATE TABLE IF NOT EXISTS cities
              location TEXT NOT NULL);''')
 
 opisenie.commit()
-opisenie_cursor.execute("SELECT name, description, location FROM cities")
-rows = opisenie_cursor.fetchall()
-f = 0
-for row in rows:
-    f+=1
-home_masive = []
-mas = []
-for i in range(f):
-    home_masive = mas
-
 
 # add_city_opis("Санкт-Петербург", "Город на Неве", "Северо-Западная Россия")
 # opisenie.commit()
@@ -86,45 +76,45 @@ for i in range(f):
 # for row in rows:
 #     f = row[0]
 # print(f)
-customtkinter.set_appearance_mode("light")
-customtkinter.set_default_color_theme("dark-blue")
-class App(customtkinter.CTk):
+set_appearance_mode("light")
+set_default_color_theme("dark-blue")
+class App(CTk):
 
     def __init__(self):
         super().__init__()
         screen_width = self.winfo_screenwidth()
         image_path = os.path.join(os.path.dirname(
             os.path.realpath(__file__)), "test_images")
-        self.logo_image = customtkinter.CTkImage(Image.open(
+        self.logo_image = CTkImage(Image.open(
             os.path.join(image_path, "CustomTkinter_logo_single.png")),
             size=(26, 26))
-        self.home_image = customtkinter.CTkImage(light_image=Image.open(
+        self.home_image = CTkImage(light_image=Image.open(
             os.path.join(image_path, "home_dark.png")),
             dark_image=Image.open(os.path.join(image_path, "home_light.png")),
             size=(20, 20))
-        self.chat_image = customtkinter.CTkImage(light_image=Image.open(
+        self.chat_image = CTkImage(light_image=Image.open(
             os.path.join(image_path, "chat_dark.png")),
             dark_image=Image.open(os.path.join(image_path, "chat_light.png")),
             size=(20, 20))
-        self.add_user_image = customtkinter.CTkImage(
+        self.add_user_image = CTkImage(
             light_image=Image.open(os.path.join(image_path,
                                 "add_user_dark.png")),
             dark_image=Image.open(
             os.path.join(image_path, "add_user_light.png")), size=(20, 20))
-        self.galochka_image = customtkinter.CTkImage(light_image=Image.open(
+        self.galochka_image = CTkImage(light_image=Image.open(
             os.path.join(image_path, "Check_mark_23x20_02.svg.png")),
             dark_image=Image.open(os.path.join(image_path, "Check_mark_23x20_02_white.svg.png")),
             size=(20, 20))
-        self.plase_image = customtkinter.CTkImage(light_image=Image.open(
+        self.plase_image = CTkImage(light_image=Image.open(
             os.path.join(image_path, "900364.png")),
             dark_image=Image.open(os.path.join(image_path, "900364_white.png")),
             size=(20, 20))
 
         p = open('file(sgl)/Логин', 'r', encoding="UTF-8")
-        self.login_frame = customtkinter.CTkFrame(self)
-        self.login_label = customtkinter.CTkLabel(self.login_frame,
+        self.login_frame = CTkFrame(self)
+        self.login_label = CTkLabel(self.login_frame,
                                                   text="Регистрация/Вход",
-            font=customtkinter.CTkFont(size=30, weight="bold"))
+            font=CTkFont(size=30, weight="bold"))
         self.title("Богадство мира")
         if p.read() == '':
             self.grid_rowconfigure(0, weight=1)
@@ -135,13 +125,13 @@ class App(customtkinter.CTk):
             , sticky="nsew")
             self.login_label.grid(row=0, column=0,
             padx=screen_width//2 - 160, pady=(150, 15))
-            self.username_entry = customtkinter.CTkEntry(self.login_frame,
+            self.username_entry = CTkEntry(self.login_frame,
             width=200, placeholder_text="Логин",)
             self.username_entry.grid(row=1, column=0, padx=30, pady=(15, 15))
-            self.password_entry = customtkinter.CTkEntry(self.login_frame,
+            self.password_entry = CTkEntry(self.login_frame,
             width=200, show="*", placeholder_text="Пароль")
             self.password_entry.grid(row=2, column=0, padx=30, pady=(0, 15))
-            self.login_button = customtkinter.CTkButton(self.login_frame
+            self.login_button = CTkButton(self.login_frame
             , text="Войти", command=self.main, width=200)
             self.login_button.grid(row=3, column=0, padx=30, pady=(15, 15))
         else:
@@ -190,17 +180,17 @@ class App(customtkinter.CTk):
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=1)
         # Навигация
-        self.navigation_frame = customtkinter.CTkFrame(self, corner_radius=0)
+        self.navigation_frame = CTkFrame(self, corner_radius=0)
         self.navigation_frame.grid(row=0, column=0, sticky="nsew")
         self.navigation_frame.grid_rowconfigure(6, weight=1)
-        self.navigation_frame_label = customtkinter.CTkLabel(
+        self.navigation_frame_label = CTkLabel(
                 self.navigation_frame,
                 text="  Image Example",
                 image=self.logo_image, compound="left",
-                font=customtkinter.CTkFont(size=15, weight="bold"))
+                font=CTkFont(size=15, weight="bold"))
         self.navigation_frame_label.grid(row=0, column=0, padx=20, pady=20)
         # 1 фрейм
-        self.home_button = customtkinter.CTkButton(self.navigation_frame,
+        self.home_button = CTkButton(self.navigation_frame,
                 corner_radius=0,
                 height=40, border_spacing=10,
                 text=FRAMGL_NAME,
@@ -210,7 +200,7 @@ class App(customtkinter.CTk):
                 command=self.home_button_event)
         self.home_button.grid(row=1, column=0, sticky="ew")
         # 2 фрейм
-        self.frame_2_button = customtkinter.CTkButton(self.navigation_frame,
+        self.frame_2_button = CTkButton(self.navigation_frame,
                 corner_radius=0, height=40,
                 border_spacing=10, text=FRAM1_NAME, fg_color="transparent",
                 text_color=("gray10", "gray90"),
@@ -219,7 +209,7 @@ class App(customtkinter.CTk):
                 command=self.frame_2_button_event)
         self.frame_2_button.grid(row=2, column=0, sticky="ew")
         # 3 фрейм
-        self.frame_3_button = customtkinter.CTkButton(
+        self.frame_3_button = CTkButton(
                 self.navigation_frame, corner_radius=0, height=40,
                 border_spacing=10, text=FRAM2_NAME,
                 fg_color="transparent", text_color=("gray10", "gray90"),
@@ -228,7 +218,7 @@ class App(customtkinter.CTk):
                 command=self.frame_3_button_event)
         self.frame_3_button.grid(row=3, column=0, sticky="ew")
         # 4 фрейм
-        self.frame_4_button = customtkinter.CTkButton(
+        self.frame_4_button = CTkButton(
             self.navigation_frame, corner_radius=0, height=40,
             border_spacing=10, text=FRAM3_NAME,
             fg_color="transparent", text_color=("gray10", "gray90"),
@@ -237,7 +227,7 @@ class App(customtkinter.CTk):
             command=self.frame_4_button_event)
         self.frame_4_button.grid(row=4, column=0, sticky="ew")
         # 5 фрейм
-        self.frame_5_button = customtkinter.CTkButton(
+        self.frame_5_button = CTkButton(
             self.navigation_frame, corner_radius=0, height=40,
             border_spacing=10, text=FRAM4_NAME,
             fg_color="transparent", text_color=("gray10", "gray90"),
@@ -246,7 +236,7 @@ class App(customtkinter.CTk):
             command=self.frame_5_button_event)
         self.frame_5_button.grid(row=5, column=0, sticky="ew")
         # выбор темы
-        self.appearance_mode_menu = customtkinter.CTkOptionMenu(
+        self.appearance_mode_menu = CTkOptionMenu(
                 self.navigation_frame,
                 values=["Светлая", "Тёмная", "Системная"],
                 command=self.change_appearance_mode_event)
@@ -254,66 +244,66 @@ class App(customtkinter.CTk):
                 column=0, padx=20, pady=20, sticky="s")
 
         # Главный фрейм
-        self.home_frame = customtkinter.CTkFrame(self,
+        self.home_frame = CTkFrame(self,
                 corner_radius=0, fg_color="transparent")
         self.home_frame.grid_columnconfigure(0, weight=1)
 
         screen_height = self.winfo_screenheight()
 
-        self.home_Lable = customtkinter.CTkLabel(self.home_frame,
-        text='Главная', font=customtkinter.CTkFont(size=40, weight="bold"))
+        self.home_Lable = CTkLabel(self.home_frame,
+        text='Главная', font=CTkFont(size=40, weight="bold"))
         self.home_Lable.grid()
 
-        self.home_None = customtkinter.CTkLabel(self.home_frame,
+        self.home_None = CTkLabel(self.home_frame,
         text='',)
         self.home_None.grid(row=1, column=0)
-        self.home_None = customtkinter.CTkLabel(self.home_frame,
+        self.home_None = CTkLabel(self.home_frame,
                                                 text='', )
         self.home_None.grid(row=2, column=0)
-        self.home_None = customtkinter.CTkLabel(self.home_frame,
+        self.home_None = CTkLabel(self.home_frame,
                                                 text='', )
         self.home_None.grid(row=3, column=0)
 
-        self.home_Lable_all = customtkinter.CTkLabel(self.home_frame,
+        self.home_Lable_all = CTkLabel(self.home_frame,
         text='Город         Название'
              '              Описание        Уже был       '
              'Не хочу быть',
-            font=customtkinter.CTkFont(size=30, weight="bold"))
+            font=CTkFont(size=30, weight="bold"))
         self.home_Lable_all.grid(row=4, column=0)
 
-        tk_textbox = customtkinter.CTkScrollableFrame(self.home_frame)
+        tk_textbox = CTkScrollableFrame(self.home_frame)
         tk_textbox.grid(row=5, column=0, sticky="nsew")
         tk_textbox.configure(height=screen_height-250)
 
 
         # создаем 3 фрейм
-        self.second_frame = customtkinter.CTkFrame(self, corner_radius=0,
+        self.second_frame = CTkFrame(self, corner_radius=0,
             fg_color="transparent")
         # Настройка
-        self.navigation_frame2_label = customtkinter.CTkLabel(
+        self.navigation_frame2_label = CTkLabel(
                 self.second_frame,
                 text=LOGIN,
-                font=customtkinter.CTkFont(size=30, weight="bold"))
+                font=CTkFont(size=30, weight="bold"))
         self.navigation_frame2_label.grid(padx=200, pady=100)
         # создаем 2 фрейм
-        self.third_frame = customtkinter.CTkFrame(self, corner_radius=0,
+        self.third_frame = CTkFrame(self, corner_radius=0,
             fg_color="transparent")
         # создаём 4 фрейм
-        self.negative_frame = customtkinter.CTkFrame(self, corner_radius=0,
+        self.negative_frame = CTkFrame(self, corner_radius=0,
                                                    fg_color="transparent")
         # создаём 5 фрейм
-        self.pozitive_frame = customtkinter.CTkFrame(self, corner_radius=0,
+        self.pozitive_frame = CTkFrame(self, corner_radius=0,
                                                      fg_color="transparent")
         # Настройка
-        self.navigation_frame_label = customtkinter.CTkLabel(self.third_frame,
+        self.navigation_frame_label = CTkLabel(self.third_frame,
             text="Введите название достопремечательности и через / описание",
-                font=customtkinter.CTkFont(size=30, weight="bold"))
+                font=CTkFont(size=30, weight="bold"))
         self.navigation_frame_label.grid(padx=200, pady=100)
-        self.home_frame_frame_2_entry = customtkinter.CTkEntry(
+        self.home_frame_frame_2_entry = CTkEntry(
             self.third_frame, width=1000, height=50)
         self.home_frame_frame_2_entry.grid(row=1, column=0,
                                     padx=200, pady=10, sticky="nsw")
-        self.bitin_2frame = customtkinter.CTkButton(self.third_frame
+        self.bitin_2frame = CTkButton(self.third_frame
             , text='Отправить на проверку', height=50)
         self.bitin_2frame.grid(row=2, column=0, padx=200, pady=10)
         # Главный фрейм
@@ -323,14 +313,29 @@ class App(customtkinter.CTk):
     def frame_3_button_event(self):self.select_frame_by_name(FRAM2_NAME)
     def frame_4_button_event(self):self.select_frame_by_name(FRAM3_NAME)
     def frame_5_button_event(self):self.select_frame_by_name(FRAM4_NAME)
+    def home_masive_install(self):
+        opisenie_cursor.execute("SELECT name, description, location FROM cities")
+        rows = opisenie_cursor.fetchall()
+        f = 0
+        for _ in rows:
+            f += 1
+        self.home_masive = []
+        self.mas = [1,2,3,4,5]
+        for i in range(f):
+            self.home_masive.append(self.mas)
+            self.home_masive[i][0] = CTkLabel(self, text='dsfdfsdf')
+            self.home_masive[i][1] = CTkLabel(self, text='dsfdfsdf')
+            self.home_masive[i][2] = CTkLabel(self, text='dsfdfsdf')
+            self.home_masive[i][3] = CTkButton(self, text='dsfdfsdf')
+            self.home_masive[i][4] = CTkButton(self, text='dsfdfsdf')
     @staticmethod
     def change_appearance_mode_event(new_appearance_mode):
         if new_appearance_mode == 'Светлая':
-            customtkinter.set_appearance_mode("light")
+            set_appearance_mode("light")
         elif new_appearance_mode == 'Тёмная':
-            customtkinter.set_appearance_mode("dark")
+            set_appearance_mode("dark")
         else:
-            customtkinter.set_appearance_mode("system")
+            set_appearance_mode("system")
     # не трож убьёт
     def avtor(self, password, file):
         if password == '':
@@ -348,8 +353,8 @@ class App(customtkinter.CTk):
                     return 0
                 else:
                     self.login_label1.grid_forget()
-                    self.login_label1 = customtkinter.CTkLabel(self.login_frame, text="Пароль или логин\nне правильный",
-                         font=customtkinter.CTkFont(size=20, weight="bold"))
+                    self.login_label1 = CTkLabel(self.login_frame, text="Пароль или логин\nне правильный",
+                         font=CTkFont(size=20, weight="bold"))
                     self.login_label1.grid(row=4, column=0)
                     return 1
         file.write(str(int(g) + 1) + '\t' + LOGIN + '\t' + password + '\t' + 'f' + '\n')
@@ -361,9 +366,9 @@ class App(customtkinter.CTk):
         return 0
     def login_event(self):
         global g,LOGIN
-        self.login_label1 = customtkinter.CTkLabel(self.login_frame
+        self.login_label1 = CTkLabel(self.login_frame
             , text="",
-             font=customtkinter.CTkFont(size=20, weight="bold"))
+             font=CTkFont(size=20, weight="bold"))
         self.login_label1.grid(row=4, column=0)
         p = open('file(sgl)/Логин', 'r', encoding="UTF-8")
         if p.read() == '':
