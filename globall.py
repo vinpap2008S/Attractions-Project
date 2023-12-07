d7b407eea4f0542bb16d6eee55b0ca7d5146fbc8
vinpap2008S <mirzoevsergej881@gmail.com>
2023-12-07 17:25:32 +0300

    приложение 4

--- globall.py
+++ globall.py
@@ -3,10 +3,11 @@ FRAM1_NAME = "Добавление обьектов"
 FRAM2_NAME = "Профиль"
 FRAM3_NAME = "Уже был"
 FRAM4_NAME = "Не хочу быть"
+"""
 opisenie = sqlite3.connect('file(sgl)/opisenie.db')
 opisenie_cursor = opisenie.cursor()
 while True:
-    citi = input('Город\n')
+    citi = Москва #input('Город\n')
     name = input('Название\n')
     opis = input('Описание\n')
     vid = input('Вид\n')
@@ -19,3 +20,4 @@ while True:
                  location TEXT NOT NULL);''')
     add_city_opis("Санкт-Петербург", "Город на Неве", "Северо-Западная Россия", "")
 opisenie.commit()
+"""
